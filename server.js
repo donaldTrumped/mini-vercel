cef349899bb25af45007f4133ee7dc10a11e5830
davidM <git@7d.au>
2026-07-21 20:00:41 +1000

    GET-only

--- server.js
+++ server.js
@@ -2,16 +2,10 @@ require("dotenv").config();
 
 const express = require("express");
 const { MongoClient } = require("mongodb");
+
 const app = express();
 
-app.use(express.json());
-app.use(express.static(__dirname));
-
 const client = new MongoClient(process.env.MONGODB_URI);
-
-app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/index.html");
-});
 
 async function connectDB() {
     if (!client.topology || !client.topology.isConnected()) {
@@ -19,27 +13,21 @@ async function connectDB() {
     }
 }
 
+app.use(express.static(__dirname));
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
 app.get("/api/message", async function (req, res) {
+    await connectDB();
+
     const database = client.db("mini_vercel_db");
     const collection = database.collection("messages");
 
     const message = await collection.findOne();
 
     res.json(message);
-});
-
-app.get("/api/message", async function (req, res) {
-
-    await connectDB();
-    const database = client.db("mini_vercel_db");
-    const collection = database.collection("messages");
-
-    await collection.updateOne(
-        {},
-        { $set: { message: req.body.message } }
-    );
-
-    res.json({ success: true });
 });
 
 app.listen(3000, function () {
