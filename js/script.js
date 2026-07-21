0f9bac94cfa45938863af5699159057c9d97c5e3
davidM <git@7d.au>
2026-07-21 20:36:39 +1000

    smallapp

--- js/script.js
+++ js/script.js
@@ -1,28 +1,7 @@
-const button = document.getElementById("myButton");
-const input = document.getElementById("messageInput");
 const message = document.getElementById("message");
-
-button.addEventListener("click", async function () {
 
 fetch("/api/message")
     .then(response => response.json())
     .then(data => {
-        document.getElementById("message").textContent = data.message;
+        message.textContent = data.message;
     });
-
-
-    await fetch("/api/message", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            message: input.value
-        })
-    });
-
-    const response = await fetch("/api/message");
-    const data = await response.json();
-
-    message.textContent = data.message;
-});
