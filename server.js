a57275fb362c4c9ec5081980afffb3b5ac8f5fb9
davidM <git@7d.au>
2026-07-21 21:07:33 +1000

    Move frontend to public for Vercel

--- server.js
+++ server.js
@@ -14,10 +14,10 @@ async function connectDB() {
     }
 }
 
-app.use(express.static(__dirname));
+app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/index.html");
+res.sendFile(__dirname + "/public/index.html");
 });
 
 app.get("/api/message", async function (req, res) {
